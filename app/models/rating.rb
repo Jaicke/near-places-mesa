27927b424d1755595f943eaff927e0466f8a3076
Jaicke Santos <jaicke-santos@outlook.com>
2020-02-27 08:11:58 -0300

    Add models and errors pt-BR locale.

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -2,7 +2,7 @@ class Rating < ApplicationRecord
   belongs_to :user
   belongs_to :place
 
-  validates_presence_of :note, :comment
+  validates_presence_of :note
   validates_numericality_of :note, greater_than_or_equal_to: 0, less_than_or_equal_to: 5
 
 end
