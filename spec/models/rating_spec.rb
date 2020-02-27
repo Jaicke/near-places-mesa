f1b588c4e3d8de08f232258c497db9dd38f48845
Jaicke Santos <jaicke-santos@outlook.com>
2020-02-27 08:16:56 -0300

    Remove rating comment validation from test.

--- spec/models/rating_spec.rb
+++ spec/models/rating_spec.rb
@@ -10,7 +10,6 @@ RSpec.describe Rating, type: :model do
 
   describe 'validations' do
     it { should validate_presence_of(:note) }
-    it { should validate_presence_of(:comment) }
     it { should validate_numericality_of(:note).is_greater_than_or_equal_to(0)}
     it { should validate_numericality_of(:note).is_less_than_or_equal_to(5)}
   end  
